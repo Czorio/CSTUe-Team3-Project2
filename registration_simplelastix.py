284d4287c959b18e88f3b74e350c507abc95d5cd
Mathijs de Boer <Czorio@users.noreply.github.com>
2019-03-29 15:54:00 +0100

    Update registration_simplelastix.py

--- registration_simplelastix.py
+++ registration_simplelastix.py
@@ -1,102 +1,114 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 18 17:49:19 2019
+Spyder Editor
 
-@author: lxs
+This is a temporary script file.
 """
-
-import skimage
-import SimpleITK
-import matplotlib.pyplot as plt
+#%% import packages
 import numpy as np
+import os
+import SimpleITK as sitk
 
-#%% Read in the fixed image and moving image and atlas.
-fixedImage = SimpleITK.ReadImage(r'.\TrainingData\p102\mr_bffe.mhd')
-movingImage = SimpleITK.ReadImage(r'.\TrainingData\p107\mr_bffe.mhd')
-label = SimpleITK.ReadImage(r'.\TrainingData\p107\prostaat.mhd')
+from skimage.morphology import dilation, ball
 
-#%% Set the parameters
-parameterVec = SimpleITK.VectorOfParameterMap()
-parameterVec.append(SimpleITK.GetDefaultParameterMap('affine'))
-parameterVec.append(SimpleITK.GetDefaultParameterMap('bspline'))
+#%% Read patient images and segmentations; store into two lists
+folder = r'C:\Users\czori\Downloads\TrainingData\TrainingData'
+subdirs = [x[0] for x in os.walk(folder)]
+images = []
+labels = []
+for subdir in subdirs[1:]:
+    # Skip the first entry, as that is the base directory and has no images of its own
+    if "results" in subdir:
+        continue
+    
+    fpath = os.path.join(folder, subdir)
+    images.append(sitk.ReadImage(os.path.join(fpath, "mr_bffe.mhd")))
+    labels.append(sitk.ReadImage(os.path.join(fpath, "prostaat.mhd")))
 
-#%% Use the package and do the registration
-SimpleElastix = SimpleITK.SimpleElastix()
-SimpleElastix.LogToConsoleOn()
+print("\nThe dataset consists of {} images".format(len(images)))
+print("Each image has size: ", images[0].GetSize())
 
-SimpleElastix.SetFixedImage(fixedImage)
-SimpleElastix.SetMovingImage(movingImage)
+#%% Select atlasses and validation images
+atlasses = images[:9]
+atlas_labels = labels[:9]
+validation = images[10:]
+validation_labels = labels[10:]
+print("{} atlasses, {} validation images".format(len(atlasses), len(validation)))
+#%% For each image, register atlasses
+parameterVec = sitk.VectorOfParameterMap()
+parameterVec.append(sitk.GetDefaultParameterMap('affine'))
+parameterVec.append(sitk.GetDefaultParameterMap('bspline'))
 
-SimpleElastix.SetParameterMap(parameterVec)
+# Simple counter
+ctr = 1
 
-SimpleElastix.Execute()
+for image, label in zip(validation, validation_labels):
+    result_labels = sitk.VectorOfImage()
+    
+    selx = sitk.SimpleElastix()
+    selx.LogToConsoleOn()
+    
+    selx.SetFixedImage(image)
+    selx.SetParameterMap(parameterVec)
+    
+    # Use all atlasses
+    for atlas, atlas_label in zip(atlasses, atlas_labels):
+        selx.SetMovingImage(atlas)
+        selx.Execute()
+        
+        # Apply the transform found during registration to the label
+        result_labels.push_back(sitk.Transformix(atlas_label, selx.GetTransformParameterMap()))
+        
+    # This converts the result labels from a SITK VectorOfImage to a np_array, it's a little
+    # in a roundabout way, but there is no dedicated method to do this
+    label_heatmap = sitk.GetArrayFromImage(result_labels[0])    
+    for result_label in result_labels[1:]:
+        label_heatmap += sitk.GetArrayFromImage(result_label)
+    
+    label_vote = sitk.LabelVoting(result_labels)
+        
+    # Convert to a heatmap/probability map
+    label_heatmap = sitk.GetImageFromArray(label_heatmap)
+    label_heatmap.CopyInformation(image)
+    
+    # Make sure path exists that we can write to
+    if not os.path.exists(os.path.join(folder, "results", str(ctr))):
+        os.makedirs(os.path.join(folder, "results", str(ctr)))
+    
+    # Save to disk
+    print("Saving initial results")
+    sitk.WriteImage(image, os.path.join(folder, "results", str(ctr), "image.mhd"))
+    sitk.WriteImage(label_heatmap, os.path.join(folder, "results", str(ctr), "label_heatmap.mhd"))
+    sitk.WriteImage(label_vote, os.path.join(folder, "results", str(ctr), "label_voting.mhd"))
+    
+    # Get ROI for zooming
+    print("Generating ROI")
+    roi = sitk.GetArrayFromImage(label_vote)
+    target_size = np.sum(roi) * 1.5
+    while np.sum(roi) < target_size:
+        roi = dilation(roi)
+        print(np.sum(roi), target_size, end="\r")
+    print("Dilating complete")
+    roi = sitk.GetImageFromArray(roi)
+    roi.CopyInformation(image)
+    sitk.WriteImage(roi, os.path.join(folder, "results", str(ctr), "ROI.mhd"))
+    
+    # Second registration with mask
+    result_labels = sitk.VectorOfImage()
+    
+    selx = sitk.SimpleElastix()
+    selx.LogToConsoleOn()
+    
+    selx.SetFixedImage(image)
+    selx.SetFixedMask(roi)
+    for atlas, atlas_label in zip(atlasses, atlas_labels):
+        selx.SetMovingImage(atlas)
+        selx.Execute()
+        
+         # Apply the transform found during registration to the label
+        result_labels.push_back(sitk.Transformix(atlas_label, selx.GetTransformParameterMap()))
+        
+    label_vote = sitk.LabelVoting(result_labels)
+    sitk.WriteImage(label_vote, os.path.join(folder, "results", str(ctr), "final_label_voting.mhd"))
 
-#%% Reverse the pervious process to the atlas in moving image
-resultLabels = SimpleITK.VectorOfImage()
-
-reverse = SimpleElastix.GetTransformParameterMap()
-resultLabels.push_back(SimpleITK.Transformix(label, reverse))
-
-fixedLabel = SimpleITK.LabelVoting(resultLabels) 
-
-#%% Save the reslut image
-result = SimpleElastix.GetResultImage()
-SimpleITK.WriteImage(result,'result.mhd')
-SimpleITK.WriteImage(fixedLabel,'label.mhd')
-
-#%% Plot the image
-fixedImage = SimpleITK.GetArrayFromImage(fixedImage)
-plt.figure(1)
-plt.title('The fixed image')
-plt.imshow(np.sum(fixedImage, axis = 0), cmap='gray')
-
-movingImage = SimpleITK.GetArrayFromImage(movingImage)
-plt.figure(2)
-plt.title('The moving image')
-plt.imshow(np.sum(movingImage, axis = 0), cmap='gray')
-
-resultImg = SimpleITK.ReadImage(r'.\result.mhd')
-resultImg = SimpleITK.GetArrayFromImage(resultImg)
-plt.figure(3)
-plt.title('The result image')
-plt.imshow(np.sum(resultImg, axis = 0), cmap='gray')
-
-resultLab = SimpleITK.ReadImage(r'.\label.mhd')
-resultLab = SimpleITK.GetArrayFromImage(resultLab)
-plt.figure(4)
-plt.title('The reversed atlas')
-plt.imshow(np.sum(resultLab, axis = 0), cmap='gray')
-
-resultLab = SimpleITK.GetArrayFromImage(label)
-plt.figure(5)
-plt.title('The original atlas')
-plt.imshow(np.sum(resultLab, axis = 0), cmap='gray')
-
-
-#%% Zooming
-
-TotalVolPros = resultLab.sum()
-NewTotalVolPros = TotalVolPros*1.5
-
-print(TotalVolPros, NewTotalVolPros)
-
-Dilation = skimage.morphology.binary_dilation(resultLab)
-
-while TotalVolPros < NewTotalVolPros:
-    Dilation = skimage.morphology.binary_dilation(Dilation)
-    TotalVolPros = Dilation.sum()
-    print(TotalVolPros, NewTotalVolPros)
-
-plt.figure(6)
-plt.title('Dilation')
-plt.imshow(np.sum(Dilation, axis = 0), cmap='gray')
-
-ZoomedImage = fixedImage*Dilation
-
-plt.figure(7)
-plt.title('Zooming')
-plt.imshow(np.sum(ZoomedImage, axis = 0), cmap='gray')
-
-plt.show()
-
-#%% Validation
+    ctr += 1
